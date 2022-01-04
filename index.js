bf55bfa7550e291728b911b8b7f8846c3a77a18e
John Christopher Azcarraga <stellarm_13@yahoo.com>
2022-01-04 11:53:37 +0800

    Feat: Add env file

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ const port = process.env.port || 8000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+const enrolmentRouter = require('./routes/enrolment');
+app.use('/course/enrolment', enrolmentRouter );
+
 app.listen( port, () => { console.log(`Application is running on port ${port}.`)} );
