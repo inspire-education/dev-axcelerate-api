247ed999f63ff1021a1e784cf775b115c1e55720
John Christopher Azcarraga <stellarm_13@yahoo.com>
2022-01-04 17:02:08 +0800

    Feat: Finalizing env variables working

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app  = express();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 /* Parsing request body */
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
 
 const enrolmentRouter = require('./routes/enrolment');
 app.use('/course/enrol', enrolmentRouter );
--- routes/enrolment.js
+++ routes/enrolment.js
@@ -3,26 +3,39 @@ const axios   = require('axios');
 const router  = express.Router();
 
 router.post('/', ( request, response ) => {
+
     const newEnrolment = { 
         contactID:  request.body.contactID, 
         instanceID: request.body.instanceID,
         type: 'p'
+    } 
+   
+
+    /* For Testing */
+    /* 
+    const newEnrolment = { 
+        contactID:  12345, 
+        instanceID: 12345,
+        type: 'p'
     }
+    */
 
     const sendEnrolment = async () => {
         try {
-            return await axios.post( process.env.STAGING_URL, newEnrolment );
+            return await axios.post( process.env.STAGING_BASEURL, newEnrolment );
         }catch(e){
             /* Returns the error from the POST call */
             console.error( e );
         }
     } 
 
-    const responseNewEnrolment = sendEnrolment.then( res => {
-        console.log( res );
-    }).catch( error => response.send( { e : error }) )
+    const newEnrolmentFunction = async () => {
+        const responseNewEnrolment = sendEnrolment().then( res => {
+            console.log( res );
+        }).catch( error => response.send( { e : error }) )
+    }
 
-    responseNewEnrolment();
+    newEnrolmentFunction();
 });
 
 
