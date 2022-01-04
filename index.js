b0f5446a76d258d7cee593267e62f16254f1005e
John Christopher Azcarraga <stellarm_13@yahoo.com>
2022-01-04 11:45:27 +0800

    Feat: Adding body-parser

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app  = express();
 const port = process.env.port || 8000;
 
+/* Parsing request body */
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.listen( port, () => { console.log(`Application is running on port ${port}.`)} );
