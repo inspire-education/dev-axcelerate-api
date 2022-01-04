464953bd91b68d2b0c7b4f984c9a64d9c4c34d34
John Christopher Azcarraga <stellarm_13@yahoo.com>
2022-01-04 12:11:20 +0800

    Initial enrol route

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const port = process.env.port || 8000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const enrolmentRouter = require('./routes/enrolment');
-app.use('/course/enrolment', enrolmentRouter );
+app.use('/course/enrol', enrolmentRouter );
 
 app.listen( port, () => { console.log(`Application is running on port ${port}.`)} );
--- routes/enrolment.js
+++ routes/enrolment.js
@@ -1,8 +1,28 @@
 const express = require('express');
-const router = express.Router();
+const axios   = require('axios');
+const router  = express.Router();
 
 router.post('/', ( request, response ) => {
-    const newEnrolment = { contactID: request.body.contactID, }
+    const newEnrolment = { 
+        contactID:  request.body.contactID, 
+        instanceID: request.body.instanceID,
+        type: 'p'
+    }
+
+    const sendEnrolment = async () => {
+        try {
+            return await axios.post( process.env.STAGING_URL, newEnrolment );
+        }catch(e){
+            /* Returns the error from the POST call */
+            console.error( e );
+        }
+    } 
+
+    const responseNewEnrolment = sendEnrolment.then( res => {
+        console.log( res );
+    }).catch( error => response.send( { e : error }) )
+
+    responseNewEnrolment();
 });
 
 
