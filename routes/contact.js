9d79e4ad09f0a00c5aa9ef62ee8876a7a7f9cba0
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-08-08 22:30:32 +0800

    Fix: URL for GET Contact, Add log

--- routes/contact.js
+++ routes/contact.js
@@ -37,7 +37,7 @@ router.get('/:contactID', ( request, response ) => {
                 If user exists, we get the contactID for the enrollment
                 If user does not exist, we proceed on creating a new contact on aXcelerate 
             */
-
+            console.log(res);
             if( res.data.length > 0 ){ response.send( { contactID: res.data[0].CONTACTID }); }
             else{ response.send(false); }
 
