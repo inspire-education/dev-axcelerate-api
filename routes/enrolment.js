6f8b1f0f7134faba037ce2369509df9b142f076f
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-08-22 11:52:52 +0800

    Feat: Add logs on enrolment route

--- routes/enrolment.js
+++ routes/enrolment.js
@@ -11,7 +11,14 @@ const requestConfig = {
 
 router.post('/', ( request, response ) => {
 
-    console.log( request.body );
+    /** 
+     *  classID    - Hub Course ID
+     *  contactID  - Hub User ID 
+    **/
+
+    console.log(`${ new Date() } POST -- /enrolments `);
+    console.log(`REQUEST BODY -- ${ request.body }`);
+
     const sendEnrolment = async () => {
         try {
             return await axios.post( `${process.env.STAGING_BASEURL}/course/enrol?contactID=${request.body.contactID}&instanceID=${request.body.classID}&type=p`, null , requestConfig );
@@ -40,8 +47,10 @@ router.post('/updateCompetency', (request, response) => {
      *  contactID  - Hub User ID 
      *  instanceID - Unit ID
     **/
-    console.log("Update Competency");
-    console.log( request.body );
+   
+    console.log(`${ new Date() } POST -- /enrolments/updateCompetency `);
+    console.log(`REQUEST BODY -- ${ request.body }`);
+    
     const updateEnrolment = async () => {
         try {
             return await axios.put( `${process.env.STAGING_BASEURL}/course/enrolment?contactID=${request.body.contactID}&instanceID=${request.body.unit}&classID=${request.body.courseID}&competent=${request.body.competent}&type=s`, null , requestConfig );
